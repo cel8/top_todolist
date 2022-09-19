f84fd6f776e1e0b6c732822b7117ba9fd932633a
cel8 <alex.celotti@gmail.com>
2022-09-19 12:35:53 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# top_todolist
+A TODO list web page part of <a href="https://www.theodinproject.com/lessons/node-path-javascript-todo-list">The Odin Project assignment</a>.
+
+Learning:
+- %%learn%%
+
+Live demo <a href="https://cel8.github.io/top_todolist/">here</a>.
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index:              './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'WebPage Title',
+      template: './src/index.html',
+      favicon: './src/assets/icon/site.ico',
+      inject: 'body',
+      cache: false,
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  resolve: {
+    alias: {
+      Assets: path.resolve(__dirname, 'src/assets'),
+      Controller: path.resolve(__dirname, 'src/controller'),
+      Modules: path.resolve(__dirname, 'src/modules'),
+      Style: path.resolve(__dirname, 'src/style'),
+      Utilities: path.resolve(__dirname, 'src/utilities'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            // Creates `style` nodes from JS strings
+            loader: 'style-loader',
+          },
+          {
+            // Translates CSS into CommonJS
+            loader: 'css-loader',
+          }
+        ],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif|ico)$/i,
+        type: 'asset/resource',
+        // Use 'generator' to output unique name (based on webpack pattern e.g. [name], [ext], etc.)
+        generator: {
+          filename: "[name][ext][query]",
+          outputPath: "images/",
+          publicPath: "images/",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: "[name][ext][query]",
+          outputPath: "fonts/",
+          publicPath: "fonts/",
+        },
+      }
+    ],
+  },
+};
